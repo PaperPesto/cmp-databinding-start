ebe20f155fb885f5b4dd24254dfb43dec143dce5
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-11-23 23:40:49 +0100

    Commenti

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -10,6 +10,13 @@ export class ServerElementComponent implements OnInit {
   // Sto esponendo questa proprietà ai parenti di questo component
   // La bindo con la parentesi quadra e gli posso sare un alias da richiamare
   // dentro la quadra. Il binding delle proprietà dei component funziona dentro il component dell'html
+  // ----------------------------------------------------------
+  // ...parent che usa il figlio
+  // Come fare per far comunicare più component:
+  // 1 - Con questo sistema degli event emittere e dei bindng delle proprietà, ma vale
+  // solo per un solo livello di profondità nella comunicazione (padre -> filgio oppure figlio -> padre)
+  // 2 - Nel caso si debba far comunicare tra di loto più component sullo stesso livello,
+  // invece di fare figlio1 -> padre -> figlio2 si utilizzano i services
   @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
