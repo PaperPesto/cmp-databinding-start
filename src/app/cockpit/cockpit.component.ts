58d21749a1da6a08bf98205f673caa5d0c7c0123
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-11-27 08:31:38 +0100

    [Lezione 70] Server content è passato dal twb (two way binding) al local reference con ViewChild (decorator)

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,7 +10,10 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  // Qui serverContentInput viene bindato sull'elemento specifico del dom tramite il decorato ViewChild
+  // Att. non modificare il DOM con questo componente
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -19,17 +22,19 @@ export class CockpitComponent implements OnInit {
 
   onAddServer(nameInput: HTMLInputElement) {
     console.log('onAddServer: valore letto con local reference:', nameInput.value);
+    console.log('content:', this.serverContentInput);
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     console.log('onAddBlueprint: valore letto con local reference:', nameInput.value);
+    console.log('content:', this.serverContentInput);
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
